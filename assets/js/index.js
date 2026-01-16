112ee91db7b24c63f93636f045b64767f07963b9
ggustavo-souza <cavalogames1231@gmail.com>
2026-01-16 19:26:47 -0300

    Desenvolvimento: Continuação da página "Home".
    - Adição do botão gradiente com animação;
    - Adição do carroussel referente aos serviços da empresa;
    - Começo do balão de texto;

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,27 @@
 window.onload = function() {
     const fraseElement = document.querySelector('.textofrase');
-    const botaoElement = document.querySelector('.botaoSaibaMais');
+    const botaoElement = document.querySelector('.btn-grad');
     fraseElement.classList.add('is-visible');
     this.setTimeout(() => {
         botaoElement.classList.add('is-visible');
     }, 300);
 
 };
+
+function mostrartitulo() {
+    const tituloElement = document.querySelector('.servicosAba h2');
+    tituloElement.classList.add('is-visible');
+}
+
+function carroussel() {
+    const fotos = ['imagem1.jpg', 'imagem2.jpg', 'imagem3.jpg'];
+    const carrousselElement = document.querySelector('.carroussel');
+
+    let index = 0;
+    setInterval(() => {
+        console.log(index);
+        carrousselElement.innerHTML = `<img src="../images/${fotos[index]}" alt="Serviço ${index + 1}" class="imgServicos col-3 m-2 shadow-5">`;
+        index = (index + 1) % fotos.length;
+    }, 3000);
+
+}
--- index.php
+++ index.php
@@ -6,20 +6,64 @@ include HEADER_TEMPLATE;
 ?>
 
 <!-- home -->
-<main>
-    <section class="container" id="home">
-        <div class="justify-content-center align-items-center text-center">
-            <!-- frase de efeito/destaque -->
-                <div class="frase justify-content-center align-items-center text-center mt-5">
-                    <h3 class="textofrase">Inovação moldada com experiência.<br>Soluções em plástico <b>Hencel</b>, desde 1991</h3>
+<main class="container" id="home">
+    <section class="justify-content-center align-items-center text-center">
+        <!-- frase de efeito/destaque -->
+        <div class="col-12 frase justify-content-center align-items-center text-center">
+            <h3 class="textofrase">Inovação moldada com experiência.<br>Soluções em plástico <b>Hencel</b>, desde 1991
+            </h3>
+        </div>
+        <button class="btn-grad shadow-5 mt-4"><a class="linkBotao">Saiba mais</a></button>
+    </section>
+    <!-- servicos home-->
+    <section class="justify-content-center align-items-center text-center">
+        <div class="servicosAba col-12 justify-content-center align-items-center text-center">
+            <h2 class="mb-4">Nossos Serviços</h2>
+            <div class="carousel slide row justify-content-center align-items-center" id="carousel">
+                <div class="carousel-inner flex-row">
+                    <div class="carousel-item ">
+                        <img src="<?php echo BASE_URL ?>assets/images/imagem1.jpeg" alt="Imagem 1"
+                            class="imgServicos mt-2 shadow-5" width="1000px" height="700px">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>First slide label</h5>
+                            <p>Some representative placeholder content for the third slide.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item active ">
+                        <img src="<?php echo BASE_URL ?>assets/images/imagem2.jpeg" alt="Imagem 2"
+                            class="imgServicos mt-2 shadow-5" width="1000px" height="700px">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Second slide label</h5>
+                            <p>Some representative placeholder content for the third slide.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item ">
+                        <img src="<?php echo BASE_URL ?>assets/images/imagem3.jpeg" alt="Imagem 3"
+                            class="imgServicos mt-2 shadow-5" width="1000px" height="700px">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Third slide label</h5>
+                            <p>Some representative placeholder content for the third slide.</p>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Anterior</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Próximo</span>
+                    </button>
                 </div>
-            <button class="botaoSaibaMais mt-3 "><a class="linkBotao">Saiba mais</a></button>
+            </div>
         </div>
     </section>
+
+    <section class="col-12 fundoTexto">
+        <p>Temos larga experiência nos processos para injeção de plásticos como: resinas naturais, resinas
+            compostas, e de engenharia. Desde 1991 produzindo peças injetadas de plásticos com qualidade,
+            para diversos segmentos da indústria brasileira: (automobilística, eletrodomésticos, materiais
+            escolares, elétricos, automação, embalagens, entre outros). Temos parceria com ferramentarias
+            especializadas na construção e manutenção de moldes para injeção de plásticos.</p>
+    </section>
+
 </main>
-
-
-
-
-
-
